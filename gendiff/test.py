38f444d319db63be08e4ccc792ef6ffdf6607254
LAMENTIN28 <perpendomobile@gmail.com>
2024-04-21 16:50:27 +0300

    third step (optimised)

--- gendiff/test.py
+++ gendiff/test.py
@@ -1,13 +1,6 @@
 import json
 
 
-def processing(file):
-    res = []
-    for k in file:
-        res.append(k)
-    return res
-
-
 def generate_diff(path_file1, path_file2):
     file1 = json.load(open(path_file1))
     file2 = json.load(open(path_file2))
@@ -15,13 +8,12 @@ def generate_diff(path_file1, path_file2):
     all2 = []
     for key in sorted(file1 | file2):
         all2.append(key)
-    all = processing(all)
     common = file1.items() & file2.items()
-    common = processing(common)
+    common = list(common)
     unique1 = file1.items() - file2.items()
-    unique1 = processing(unique1)
+    unique1 = list(unique1)
     unique2 = file2.items() - file1.items()
-    unique2 = processing(unique2)
+    unique2 = list(unique2)
     res = ' '
     for k in all2:
         for l in common:
