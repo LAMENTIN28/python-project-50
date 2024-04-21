2ff55a921acafb09c68fbaf27d7f3f33cbb1d2b3
LAMENTIN28 <perpendomobile@gmail.com>
2024-04-21 16:36:23 +0300

    third_step

--- gendiff/test.py
+++ gendiff/test.py
@@ -4,8 +4,7 @@ import json
 def processing(file):
     res = []
     for k in file:
-        ex = k[0] + ": " + str(k[1])
-        res.append(ex)
+        res.append(k)
     return res
 
 
@@ -13,7 +12,9 @@ def generate_diff(path_file1, path_file2):
     file1 = json.load(open(path_file1))
     file2 = json.load(open(path_file2))
     all = list(set(file1.items()) | set(file2.items()))
-    all.sort(key=lambda key: key[0])
+    all2 = []
+    for key in sorted(file1 | file2):
+        all2.append(key)
     all = processing(all)
     common = file1.items() & file2.items()
     common = processing(common)
@@ -21,16 +22,21 @@ def generate_diff(path_file1, path_file2):
     unique1 = processing(unique1)
     unique2 = file2.items() - file1.items()
     unique2 = processing(unique2)
-    result = ''
-    for k in all:
-        if k in common:
-            result +=  '   ' + k + '\n'
-        elif k in unique1:
-            result +=  ' - ' + k + '\n'
-        elif k in unique2:
-            result +=  ' + ' + k + '\n'
-    result = '{\n' + result + '}'
-    print (result)
-    return result
+    res = ' '
+    for k in all2:
+        for l in common:
+            if k == l[0]:
+                res +=  "   " + l[0] + ": " + str(l[1]) + "\n"
+        for l in unique1:
+            if k == l[0]:
+                res += " - " + l[0] + ": " + str(l[1]) + "\n"
+        for l in unique2:
+            if k == l[0]:
+                res += " + " +  l[0] + ": " + str(l[1]) + "\n"
+    res = '{\n' + res[1:] + '}'
+    print (res)
+    return (res)
+
+
         
 generate_diff('gendiff/files/file1.json', 'gendiff/files/file2.json')
