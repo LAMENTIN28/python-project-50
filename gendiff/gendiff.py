2644153a4a7be838ac2ef30965a34b6b4466964e
LAMENTIN28 <perpendomobile@gmail.com>
2024-04-22 20:15:53 +0300

    end optimised

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,33 +1,31 @@
 import json
 
 
+def iter_list(key, list, par):
+    res = ""
+    for item in list:
+        if key == item[0]:
+            res += par + item[0] + ": " + str(item[1]) + "\n"
+    return res
+
+
 def generate_diff(path_file1, path_file2):
     file1 = json.load(open(path_file1))
     file2 = json.load(open(path_file2))
-    all = list(set(file1.items()) | set(file2.items()))
     all2 = []
     for key in sorted(file1 | file2):
         all2.append(key)
-    common = file1.items() & file2.items()
-    common = list(common)
-    unique1 = file1.items() - file2.items()
-    unique1 = list(unique1)
-    unique2 = file2.items() - file1.items()
-    unique2 = list(unique2)
-    res = ' '
-    for k in all2:
-        for l in common:
-            if k == l[0]:
-                res +=  "   " + l[0] + ": " + str(l[1]) + "\n"
-        for l in unique1:
-            if k == l[0]:
-                res += " - " + l[0] + ": " + str(l[1]) + "\n"
-        for l in unique2:
-            if k == l[0]:
-                res += " + " +  l[0] + ": " + str(l[1]) + "\n"
-    res = '{\n' + res[1:] + '}'
-    print (res)
-    return (res)
+    common = list(file1.items() & file2.items())
+    unique1 = list(file1.items() - file2.items())
+    unique2 = list(file2.items() - file1.items())
+    res1 = ""
+    for key in all2:
+        res1 += iter_list(key, common, "   ")
+        res1 += iter_list(key, unique1, " - ")
+        res1 += iter_list(key, unique2, " + ")
+    res1 = '{\n' + res1 + '}'
+    print(res1)
+    return res1
 
 
 generate_diff('gendiff/files/file1.json', 'gendiff/files/file2.json')
