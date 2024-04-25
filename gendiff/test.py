140eabb05b5fdd27135a908a103f0aab5f06855b
LAMENTIN28 <perpendomobile@gmail.com>
2024-04-25 19:16:58 +0300

    removing error

--- gendiff/test.py
+++ gendiff/test.py
@@ -1,34 +1,11 @@
-import json
+from gendiff import generate_diff
+from fixtures.right_ans1 import right_ans
 
 
-def generate_diff(path_file1, path_file2):
-    file1 = json.load(open(path_file1))
-    file2 = json.load(open(path_file2))
-    all = list(set(file1.items()) | set(file2.items()))
-    all2 = []
-    for key in sorted(file1 | file2):
-        all2.append(key)
-    common = file1.items() & file2.items()
-    common = list(common)
-    unique1 = file1.items() - file2.items()
-    unique1 = list(unique1)
-    unique2 = file2.items() - file1.items()
-    unique2 = list(unique2)
-    res = ' '
-    for k in all2:
-        for l in common:
-            if k == l[0]:
-                res +=  "   " + l[0] + ": " + str(l[1]) + "\n"
-        for l in unique1:
-            if k == l[0]:
-                res += " - " + l[0] + ": " + str(l[1]) + "\n"
-        for l in unique2:
-            if k == l[0]:
-                res += " + " +  l[0] + ": " + str(l[1]) + "\n"
-    res = '{\n' + res[1:] + '}'
-    print (res)
-    return (res)
+
+def test_right():
+	assert generate_diff('gendiff/files/file1.json', 'gendiff/files/file2.json') == right_ans
+	print('fist test passed')
 
 
-        
-generate_diff('gendiff/files/file1.json', 'gendiff/files/file2.json')
+test_right()
